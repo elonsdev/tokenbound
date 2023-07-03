ca77e5e331f2458d0ac8a30419537d2d49b7f9bf
elonsdev.eth <83188557+elonsdev@users.noreply.github.com>
2023-07-03 09:54:18 +0200

    delete download route

--- src/app/download/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { createReadStream } from "fs";
-import { resolve } from "path";
-
-export async function GET(req, res) {
-  const filePath = resolve("/tmp/tokenbound_accounts.csv");
-
-  createReadStream(filePath).pipe(res);
-}
